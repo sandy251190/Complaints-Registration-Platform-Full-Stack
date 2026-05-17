d2cf050bfd9aabe0afe37964fa42c2291d78d780
Sandeep <practicalwork25@gmail.com>
2026-05-18 00:30:40 +0530

    Deplyment

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,4 +1,4 @@
-const BACKEND_BASE_URL = 'http://localhost:3000';
+const BACKEND_BASE_URL = 'https://complaints-registration-platform-full-4lxq.onrender.com';
 const API_URL = `${BACKEND_BASE_URL}/api`;
 
 // State
