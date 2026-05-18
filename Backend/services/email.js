164fa70ce9af89d056d3e2363e4609c88490d2ed
Sandeep <practicalwork25@gmail.com>
2026-05-18 12:37:59 +0530

    otp 4

--- Backend/services/email.js
+++ Backend/services/email.js
@@ -1,8 +1,6 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const nodemailer = require('nodemailer');
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
