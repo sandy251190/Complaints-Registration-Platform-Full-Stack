e063115e268c093979afa7f3a052d5a061efb2a7
Sandeep <practicalwork25@gmail.com>
2026-05-18 12:08:45 +0530

    OPT problem

--- Backend/services/email.js
+++ Backend/services/email.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS // App Password
